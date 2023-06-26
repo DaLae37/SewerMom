c30d67d67624415f557b5d66166475874dcd068f
shintom1222 <shintom1222@gmail.com>
2023-06-26 16:06:04 +0900

    sidedoor complete

--- Assets/Scripts/GameScene/MapController.cs
+++ Assets/Scripts/GameScene/MapController.cs
@@ -20,7 +20,10 @@ public class MapController : MonoBehaviour
     public GameObject passwardroom;
     public GameObject passwarduproom;
     private SpriteRenderer sprite;
-
+    public GameObject firstdoor;
+    public GameObject traindoor;
+    public GameObject mainrightdoor;
+    public GameObject mainrightrightdoor;
     // Start is called before the first frame update
     void Start()        //새로운 방 추가될때마다 추가해주기. 초기에는 모든게 비활
     {
@@ -29,6 +32,13 @@ public class MapController : MonoBehaviour
         setmainroom(false);
 
         setmainrightroom(false);
+        setrightroom(false);
+        // door
+        setdoor(false, firstdoor);
+        setdoor(false, traindoor);
+        setdoor(false, mainrightdoor);
+        setdoor(false, mainrightrightdoor);
+        
     }
 
     // Update is called once per frame
@@ -39,16 +49,28 @@ public class MapController : MonoBehaviour
             case 1:
                 setfirstroom(true);
                 settrainroom(false);
+
+                setdoor(true,firstdoor);
                 break;
             case 2:
+                
                 setfirstroom(false);
                 settrainroom(true);
                 setmainroom(false);
+
+                setdoor(true, firstdoor);
+                setdoor(true, traindoor);
+                setdoor(false, mainrightdoor);
                 break;
             case 3:
                 settrainroom(false);
                 setmainroom(true);
                 setmainrightroom(false);
+
+                setdoor(false, firstdoor);
+                setdoor(true, traindoor);
+                setdoor(true, mainrightdoor);
+                setdoor(false, mainrightrightdoor);
                 break;
             case 4:
 
@@ -57,9 +79,43 @@ public class MapController : MonoBehaviour
             case 5:
                 setmainroom(false);
                 setmainrightroom(true);
+                setrightroom(false);
+
+                setdoor(false, traindoor);
+                setdoor(true,mainrightdoor);
+                setdoor(true, mainrightrightdoor);
+                break;
+            case 6:
+
+                break;
+            case 7:
+                setmainrightroom(false);
+                setrightroom(true);
+
+                setdoor(false, mainrightdoor);
+                setdoor(true, mainrightrightdoor);
                 break;
         }
     }
+    // 문 설정
+    private void setdoor(bool active, GameObject door)
+    {
+        if (active)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                door.transform.GetChild(i).gameObject.GetComponent<SpriteRenderer>().enabled = true;
+            }
+        }
+        else
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                door.transform.GetChild(i).gameObject.GetComponent<SpriteRenderer>().enabled = false;
+            }
+        }
+    }
+    // 방 설정
     private void setfirstroom(bool active)
     {
         sprite = firstroom.GetComponent<SpriteRenderer>();
@@ -133,5 +189,26 @@ public class MapController : MonoBehaviour
             }
         }
     }
+    private void setrightroom(bool active)
+    {
+        sprite = rightroom.GetComponent<SpriteRenderer>();
+        if (active)
+        {
+            sprite.enabled = true;
+            for (int i = 0; i < 1; i++)
+            {
+                rightroom.transform.GetChild(i).gameObject.SetActive(true);
+            }
+        }
+        else
+        {
+            sprite.enabled = false;
+            for (int i = 0; i < 1; i++)
+            {
+                rightroom.transform.GetChild(i).gameObject.SetActive(false);
+            }
+        }
+    }
+
 }
 
